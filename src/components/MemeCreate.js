c42666e21a34456fed777cb74cbb6d87bba5e762
Akshath <akshathmahajan13@gmail.com>
2020-09-27 18:27:23 +0530

    Finished MemeCreate component
    Finished design for MemeCreate component.
    Added post request to api in MemeCreate component using forms

--- src/components/MemeCreate.js
+++ src/components/MemeCreate.js
@@ -1,7 +1,8 @@
-import { makeStyles, Typography } from '@material-ui/core';
+import { Button, Grid, makeStyles, Typography } from '@material-ui/core';
 import React, {useState} from 'react'
 import { useParams } from 'react-router-dom'
 import MemeInput from './MemeInput'
+import axios from 'axios'
 const useStyles = makeStyles((theme) => ({
     root: {
             backgroundColor: theme.palette.background.default,
@@ -19,17 +20,28 @@ function MemeCreate() {
         temp[id] = value
         return _setTextVals(temp)
     }
+    const submit = () =>  {
+        var form = new FormData();
+        form.append('username', 'akshath')
+        form.append('password', 'akshath123')
+        form.append('template_id', id)
+        for(let i=0; i<boxes; i++){form.append(`boxes[${i}][text]`,textVals[i]);}
+        axios({
+            method: 'POST',
+            url: 'https://api.imgflip.com/caption_image', 
+            data: form,
+            headers: {'Content-Type':'multipart/form-data'}
+        }).then((response) => console.log(response.data))
+    };
     let arr = []
-    for(let i = 0; i < parseInt(boxes); i++){
-        arr.push(
-            <MemeInput key={i} id={i} callback={setTextVals} />
-            
-        )
-    }
+    for(let i = 0; i < parseInt(boxes); i++){arr.push(<MemeInput key={i} id={i} callback={setTextVals} />)}
     return (
         <div className={classes.root}>
             <Typography align="center" color='textPrimary' variant="h1">Meme #{id}</Typography> 
-            {arr}
+            <div>{arr}</div>
+            <Grid container justify = "center">
+                <Button type="button" onClick={submit} variant="outlined" size="large"> Make Meme </Button>
+            </Grid>
         </div>
     )
 }
