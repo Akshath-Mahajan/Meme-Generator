10162ad34a596ae65cd53f1576d1b8c1e79e0dcb
Akshath <akshathmahajan13@gmail.com>
2020-09-28 08:25:35 +0530

    Finished loading meme on page after api call

--- src/components/MemeCreate.js
+++ src/components/MemeCreate.js
@@ -7,6 +7,12 @@ const useStyles = makeStyles((theme) => ({
     root: {
             backgroundColor: theme.palette.background.default,
             height:'100vh'
+    },
+    imgContainer:{
+        width:'100%',
+        display:'flex',
+        justifyContent:'center',
+        alignContent:'center'
     }
   })
 );
@@ -15,6 +21,7 @@ function MemeCreate() {
     let {id, boxes} = useParams();
     const classes = useStyles();
     const [textVals, _setTextVals] = useState(Array.apply(null, Array(parseInt(boxes))).map(function () {return ""}))
+    const [url, setUrl] = useState(null)
     const setTextVals = (id, value)=>{
         let temp = [...textVals]
         temp[id] = value
@@ -31,17 +38,29 @@ function MemeCreate() {
             url: 'https://api.imgflip.com/caption_image', 
             data: form,
             headers: {'Content-Type':'multipart/form-data'}
-        }).then((response) => console.log(response.data))
+        }).then((response) => setUrl(response.data.data.url))
     };
     let arr = []
     for(let i = 0; i < parseInt(boxes); i++){arr.push(<MemeInput key={i} id={i} callback={setTextVals} />)}
-    return (
-        <div className={classes.root}>
+    let output = ""
+    if(url == null){
+        output = 
+        <React.Fragment>
             <Typography align="center" color='textPrimary' variant="h1">Meme #{id}</Typography> 
             <div>{arr}</div>
             <Grid container justify = "center">
                 <Button type="button" onClick={submit} variant="outlined" size="large"> Make Meme </Button>
             </Grid>
+        </React.Fragment>
+    }else{
+        output =
+            <div className={classes.imgContainer}>
+                <img src={url} style={{marginLeft:'auto', marginRight:'auto'}} alt="Please refresh the page" />
+            </div>
+    }
+    return (
+        <div className={classes.root}>
+            {output}
         </div>
     )
 }
