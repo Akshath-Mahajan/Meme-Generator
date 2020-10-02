a7176dee8673616b1bc4ee316505694e4d5f2d01
Akshath <akshathmahajan13@gmail.com>
2020-10-02 13:00:51 +0530

    Added pagination, finished project

--- src/components/MemeTemplateGrid.js
+++ src/components/MemeTemplateGrid.js
@@ -1,26 +1,50 @@
 import React, {useState, useEffect} from 'react'
 import MemeTemplate from './MemeTemplate'
 import { Grid, makeStyles } from '@material-ui/core';
+import ReactPaginate from 'react-paginate'
+import '../css/pagination.css'
 const useStyles = makeStyles((theme)=>({
     root: {backgroundColor:theme.palette.background.default}
     })
 );
 function MemeTemplateGrid() {
     const [data, setData] = useState([])
+    const [currentPage, setCurrentPage] = useState(1);
+    const [memesPerPage, setMemesPerPage] = useState(10);
+    const classes = useStyles()
     useEffect(()=>{
         fetch("https://api.imgflip.com/get_memes")
         .then(res => res.json())
         .then(data => setData(data.data.memes))
-    }, [])
-    const classes = useStyles()
+    }, []);
+    const handlePageClick = (e) => {
+        const selectedPage = e.selected + 1; //starts at 0 in ReactPaginate
+        setCurrentPage(selectedPage);
+    }
+    const last = currentPage * memesPerPage;
+    const first = last - memesPerPage;
+    const currentData = data.slice(first, last);
     return (
-        <Grid container direction="row" 
-        spacing={1}
-        justify="center"
-        alignItems="center"
-        className={classes.root}>
-            {data.map((item)=>(<MemeTemplate key={item.id} imgUrl={item.url} boxes={item.box_count} id={item.id}/>))}     
-        </Grid>
+        <div className={classes.root}>
+            <ReactPaginate 
+            previousLabel={"<"}
+            nextLabel={">"}
+            breakLabel={"..."}
+            pageCount={Math.ceil(data.length/memesPerPage)}
+            breakClassName={"break-me"}
+            containerClassName={"pagination"}
+            subContainerClassName={"pages pagination"}
+            onPageChange={handlePageClick}
+            activeClassName={"active"}
+            />
+            <Grid container direction="row" 
+            spacing={1}
+            justify="center"
+            alignItems="center">
+                {currentData.map((item)=>(<MemeTemplate key={item.id} imgUrl={item.url} boxes={item.box_count} id={item.id}/>))}     
+            </Grid>
+            
+        </div>
     )
 }
 
